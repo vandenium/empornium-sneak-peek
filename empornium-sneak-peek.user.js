4042b2b6302c754315fa0ad33b321fff78a98ebb
vandenium <86036198+vandenium@users.noreply.github.com>
2021-07-19 13:59:39 -0700

    See detail:
    - Update @match/@include, now works for requests page.
    - Simplify img src parse.
    - Only insert image if one is found.
    - Filter scripts with images up front.

--- empornium-sneak-peek.user.js
+++ empornium-sneak-peek.user.js
@@ -2,65 +2,61 @@
 // @name        Empornium Sneak Peek (ESP)
 // @description Lazy loads title images on title list pages.
 // @namespace   Empornium Scripts
-// @version     1.0.1
+// @version     1.1.0
 // @author      vandenium
 // @grant       none
-
-// ---
-// @match https://*.empornium.me/torrents.php*
-// @match https://*.empornium.me/top10.php*
-// ---
-// @match https://*.empornium.is/torrents.php*
-// @match https://*.empornium.is/top10.php*
-// ---
-// @match https://*.empornium.sx/torrents.php*
-// @match https://*.empornium.sx/top10.php*
-
+// @include /^https://www\.empornium\.(me|sx|is)\/torrents.php*/
+// @include /^https://www\.empornium\.(me|sx|is)\/top10.php*/
+// @include /^https://www\.empornium\.(me|sx|is)\/requests.php*/
 // ==/UserScript==
 
 // Changelog:
+// Version 1.1.0
+//  - Update @match/@include, now works for requests page.
+//  - Simplify img src parse.
+//  - Only insert image if one is found.
+//  - Filter scripts with images up front.
 // Version 1.0.1
 //  - Fix Brave issue.
 // Version 1.0.0
 //  - The initial version.
 // Todo:
-//  - Fix requests.
-//  - Fix Top 10 image link
 
-(function() {
+(function () {
 
-console.log('ESP Starting...');
+  console.log('ESP Starting...');
 
-const run = () => {
-  const titles = document.querySelectorAll('.torrent');
-  const scripts = document.querySelectorAll('script:not([src]):not([type])');
+  const run = () => {
+    const titles = document.querySelectorAll('.torrent, #request_table .rowa, #request_table .rowb');
+    const scripts = Array.from(document.querySelectorAll('script:not([src]):not([type])')).filter(scriptEl => scriptEl.firstChild.textContent.includes('var overlay'));
 
-  titles.forEach( (title, i) => {
-    const titleImg = title.querySelector('img');
-    titleImg.loading = 'lazy';
+    titles.forEach((title, i) => {
+      const titleImg = title.querySelector('img');
+      titleImg.loading = 'lazy';
 
-    const anchors = title.querySelectorAll('a');
-    const titleLinkAnchor = anchors[2];
-    const titleImageLink = anchors[0];
+      const anchors = title.querySelectorAll('a');
+      const titleLinkAnchor = anchors[2];
+      const titleImageLink = anchors[0];
 
-    const imgNode = document.querySelector('.leftOverlay img')
-    let imgSrc;
+      const imgNode = document.querySelector('.leftOverlay img')
+      let imgSrc;
 
-    if (imgNode) {
+      if (imgNode) {
         imgSrc = imgNode.src;
-    } else {
-        const index = !!window.navigator.brave ? i + 1 : i; // if Brave, need to shift by 1.
-        const script = scripts[index];
+      } else {
+        const script = scripts[i];
         const regex = /src=(\\".*\\")/g;
         const rawSrc = script.firstChild.textContent.match(regex)[0];
-        imgSrc = rawSrc.replace(/\\\//g, "/").replace(/\\/g, '').split('src=')[1].replace(/\"/g, '');
-    }
+        imgSrc = rawSrc.split('=')[1].replace(/\\"/g, '');
+      }
 
-    titleImg.src = imgSrc;
-    titleImg.width = 150;
-    titleImageLink.href = titleLinkAnchor.href;
-  })
-}
+      if (imgSrc !== '') {
+        titleImg.src = imgSrc;
+        titleImg.width = 150;
+        titleImageLink.href = titleLinkAnchor.href;
+      }
+    })
+  }
 
-run();
+  run();
 })();
